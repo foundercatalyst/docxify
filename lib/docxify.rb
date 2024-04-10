5add7108e31974c53045fa37915c6158298e086e
Andy Jeffries <andy@andyjeffries.co.uk>
2024-04-10 14:43:03 +0100

    Initial commit

---
+++ lib/docxify.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "docxify/version"
+
+module Docxify
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/docxify/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Docxify
+  VERSION = "0.1.0"
+end
